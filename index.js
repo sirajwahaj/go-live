146f46a3cee0b270b8f34c599feff36b61d7254e
Sirajulhaq Wahaj <siraj.wahaj@outlook.com>
2024-03-27 17:11:42 +0100

    index12

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const port = 3333;
 const express = require('express');
 const path = require('path');
+const app = express();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
