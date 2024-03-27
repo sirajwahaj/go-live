89a2a18579b6f59a20d5d0e896799163629d42e9
Sirajulhaq Wahaj <siraj.wahaj@outlook.com>
2024-03-27 17:05:14 +0100

    index12

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const port = 3333;
 const express = require('express');
-const app = express();
-app.get('/'
-    , (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(port, () => console.log('Listening on port ' + port));
